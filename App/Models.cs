7f6c21c85183877bf0c2b8c41fd80862f6f6fe98
Samuel dos Santos Oliveira <Samuel.Oliveira@fies.org.br>
2024-06-14 10:07:23 -0300

    initial

---
+++ App/Models.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Main
+    {
+       
+    }
+    
+    public class Ano
+    {
+        public Ano()
+        {
+            Dias = new List<Dia>();
+            GenerateValues();
+        }
+        
+        public List<Dia> Dias {  get; set; }
+
+        private void GenerateValues()
+        {
+
+            var dataInicial = new DateTime(DateTime.Now.Year, 1, 1);
+            for (int i = 1; i <= 365; i++) 
+            {
+                Dias.Add(new Dia(dataInicial.AddDays(i)));
+            }
+        }
+
+        public Dia GetFaturamentoMaximo()
+        {
+            return Dias.OrderByDescending(d => d.Faturamento.Valor).FirstOrDefault();
+        }
+        public Dia GetFaturamentoMinimo()
+        {
+            return Dias.OrderByDescending(d => d.Faturamento.Valor).LastOrDefault();
+        }
+
+        public int GetQtdFaturamentoAcimaMedia()
+        {
+            var media = GetMedia();
+            return Dias.Where(d => d.Faturamento.Valor > media).Count();
+        }
+
+        public decimal GetMedia()
+        {
+            var qtDiasUteis = Dias.Where(d => d.DiaUtil).Count();
+            var soma = Dias.Where(d => d.DiaUtil).Sum(d => d.Faturamento.Valor);
+            return soma/qtDiasUteis;
+        }
+
+
+
+    }
+    public class Dia
+    {
+        public Dia(DateTime diaReferencia)
+        {
+            DiaReferencia = diaReferencia;
+            DiaUtil = true;
+            Random random = new Random();
+            Faturamento = new Faturamento((decimal) random.NextDouble() * 1000);
+            if(diaReferencia.DayOfWeek == DayOfWeek.Saturday || diaReferencia.DayOfWeek == DayOfWeek.Sunday)
+            {
+                
+                DiaUtil = false;
+            }
+            else
+            {
+                //probabilidade arbitrátia de um dia ser feriado
+                var probabilidade = 0.1;
+                var valorGerado = random.NextDouble();
+                if(valorGerado <= probabilidade)
+                {
+                    DiaUtil = false;
+                } 
+            }
+        }
+        public string ToString()
+        {
+            return $"{DiaReferencia} - {DiaUtil} - {Faturamento.Valor}";
+        }
+        public DateTime DiaReferencia { get; set; }
+        public Faturamento Faturamento { get; set; }
+        public bool DiaUtil { get; set; }
+    }
+
+    public class Faturamento
+    {
+        public Faturamento(decimal valor)
+        {
+            Valor = valor;
+        }
+        public decimal Valor { get; set; }
+    }
+
+}
---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿using App;
+
+var ano = new Ano();
+
+foreach (var item in ano.Dias)
+{
+    Console.WriteLine(item.ToString());
+}
+
+var maiorFaturamento = ano.GetFaturamentoMaximo();
+var menorFaturamento = ano.GetFaturamentoMinimo();
+Console.WriteLine($"Maior Faturamento: {maiorFaturamento.ToString()}");
+Console.WriteLine($"Menor Faturamento: {menorFaturamento.ToString()}");
+Console.WriteLine($"Media: {ano.GetMedia()}");
+Console.WriteLine($"Dias faturados acima da média: {ano.GetQtdFaturamentoAcimaMedia()}");
